871152a16cfe310b54636dfafb4ae2620c4c4042
jasmith1125 <jasmith1125@yahoo.com>
2014-12-13 23:53:18 -0500

    updated settings for production database

--- app/config/database.php
+++ app/config/database.php
@@ -54,10 +54,10 @@ return array(
 
 		'mysql' => array(
 			'driver'    => 'mysql',
-			'host'      => 'chorezoo',
-			'database'  => 'root',
+			'host'      => 'localhost',
+			'database'  => 'chorezoo',
 			'username'  => 'root',
-			'password'  => '',
+			'password'  => 'CFwmvpPAN0',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
